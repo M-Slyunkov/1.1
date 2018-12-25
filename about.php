a80dfdaaba4f74d993e7d7b4f0d26efc0af56ff6
Maksim <bananadoomm@gmail.com>
2018-12-26 00:03:01 +0300

    Исправления
    1. Блок с объявлением переменных перенес в самое начало;
    2. Исправил ссылку в адресе электронной почты.

--- about.php
+++ about.php
@@ -1,3 +1,10 @@
+<?php
+$myName = 'Железный человек';
+$myAge = '26';
+$myMail = 'IRONMAN@NAMNORI.com';
+$myCity = 'Город-герой Москва';
+$aboutMe = 'Гений, миллиардер, плейбой, филантроп';
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,18 +15,12 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-    $myName = 'Железный человек';
-    $myAge = '26';
-    $myMail = 'IRONMAN@NAMNORI.com';
-    $myCity = 'Город-герой Москва';
-    $aboutMe = 'Гений, миллиардер, плейбой, филантроп';
-?>
+
 <div>
     <h2>Страница пользователя <?= $myName ?></h2>
     <p>Имя: <strong><?= $myName ?></strong></p>
     <p>Возраст: <strong><?= $myAge ?></strong></p>
-    <p>Адрес электронной почты: <strong><a href="http://www.wallpaperez.org/wallpaper/movie/Iron-Man-2008-1486.jpg"></a><?= $myMail ?></strong></p>
+    <p>Адрес электронной почты: <strong><a href="http://www.wallpaperez.org/wallpaper/movie/Iron-Man-2008-1486.jpg"><?= $myMail ?></a></strong></p>
     <p>Город: <strong><?= $myCity ?></strong></p>
     <p>О себе: <strong><?= $aboutMe ?></strong></p>
 </div>
